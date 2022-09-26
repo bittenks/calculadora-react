b6673724493a5a8255ef665d9249d80ad8ebffe5
Guilherme Bittencourt Corrêa <guilhermebittencourtcorrea@gmail.com>
2022-09-26 16:56:14 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import Calculator from './main/calculator'
+ReactDOM.createRoot(document.getElementById('root')).render(
+<>
+ <h1>Hello Calc</h1>
+ <Calculator/>
+</>
+)
