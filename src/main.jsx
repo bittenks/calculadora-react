7809ac029b0d64f367a2941c2fdc1d5f253d16c0
Guilherme Bittencourt Corrêa <guilhermebittencourtcorrea@gmail.com>
2022-09-27 15:41:49 -0300

    AddDigit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import Calculator from './main/calculator'
 ReactDOM.createRoot(document.getElementById('root')).render(
 <>
- <h1>Hello Calc</h1>
+ <h1>React Calculator ⚛</h1>
  <Calculator/>
 </>
 )
